bf3a38c61e5ea77af98d2afd67e0e99edc249639
kinghatrung <nguyenred2003@gmail.com>
2025-04-01 20:00:31 +0700

    update

--- src/pages/PublicPages/Home/Home.jsx
+++ src/pages/PublicPages/Home/Home.jsx
@@ -1,26 +1,57 @@
 import { Row, Col, Flex, Divider } from "antd";
+import { Link } from "react-router-dom";
+
+import config from "~/config";
 
 function Home() {
   return (
     <Row gutter={[20, 10]}>
-      <Col lg={17} md={24} sm={24} xs={24}>
-        <article>
+      <Col
+        className="border-b border-[#E5E5E5] pb-[20px]"
+        lg={17}
+        md={24}
+        sm={24}
+        xs={24}
+      >
+        <article className="border-b border-[#E5E5E5] pb-[20px] mb-[20px]">
           <Flex gap="20px">
-            <img className="object-cover" src="./src/assets/image/test.webp" />
-
+            <img
+              className="w-[520px] object-cover"
+              src="./src/assets/image/test.webp"
+            />
             <div>
-              <h1>Vua Bỉ thăm Hoàng thành Thăng Long</h1>
-              <p>
-                Chủ tịch nước Lương Cường và Vua Philippe cùng tham quan Hoàng
-                thành Thăng Long, nghe giới thiệu về lịch sử hàng nghìn năm của
-                dân tộc Việt Nam.
+              <h3 className="text-[24px] font-title font-medium">
+                <Link
+                  to={config.routes.home}
+                  className="hover-color text-color"
+                >
+                  Vua Bỉ thăm Hoàng thành Thăng Long
+                </Link>
+              </h3>
+              <p className="mt-[10px]">
+                <Link style={{ color: "#4f4f4f" }}>
+                  Hà Nội - Chủ tịch nước Lương Cường và Vua Philippe cùng tham
+                  quan Hoàng thành Thăng Long, nghe giới thiệu về lịch sử hàng
+                  nghìn năm của dân tộc Việt Nam.
+                </Link>
               </p>
-              <span>44' trước</span>
+              <p className="mt-2 text-[#757575]">44' trước</p>
             </div>
           </Flex>
         </article>
-        <Divider className="w-full bg-[#e5e5e5]" />
-        <div></div>
+
+        <Flex className="w-full" gap="20px">
+          <div className="w-[30%]">
+            <h3 className="text-[15px] font-title font-medium">
+              <Link to={config.routes.home}>
+                Cả nước dự kiến còn 5.000 xã phường
+              </Link>
+            </h3>
+            <Link to={config.routes.home}>
+              <img className="w-full" src="./src/assets/image/test.webp" />
+            </Link>
+          </div>
+        </Flex>
       </Col>
       <Col lg={7} md={24} sm={24} xs={24}>
         Quản cáo
